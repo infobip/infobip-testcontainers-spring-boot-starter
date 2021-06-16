9fba2e7755c83cde59368789ae6755b8b4cecdb0
tstavinoha <tin.stavinoha@infobip.com>
2021-06-16 11:19:11 +0200

    Rabbit

---
+++ infobip-rabbitmq-testcontainers-spring-boot/src/test/java/com/infobip/testcontainers/spring/rabbit/RabbitContainerWithStaticPortTest.java
@@ -0,0 +1,46 @@
+package com.infobip.testcontainers.spring.rabbit;
+
+import static org.assertj.core.api.BDDAssertions.then;
+
+import lombok.AllArgsConstructor;
+import org.assertj.core.api.BDDAssertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.boot.autoconfigure.amqp.RabbitProperties;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestConstructor;
+
+@AllArgsConstructor
+@ActiveProfiles("static-port")
+@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
+@TestConfiguration
+@SpringBootTest(classes = Main.class)
+class RabbitContainerWithStaticPortTest {
+
+    private final RabbitTemplate rabbitTemplate;
+    private final RabbitProperties rabbitProperties;
+
+    @Test
+    @DisplayName("Should initialize rabbit container and send message to exchange")
+    void shouldInitialize() {
+        // given
+        String givenMessage = "test";
+
+        // when
+        Throwable actual = BDDAssertions.catchThrowable(
+                () -> rabbitTemplate.convertAndSend("test.exchange", "test.key.bar", givenMessage));
+
+        // then
+        then(actual).isNull();
+    }
+
+    @Test
+    void shouldUseStaticPort() {
+        // then
+        then(rabbitProperties.getAddresses()).contains("localhost:5000");
+    }
+
+}
