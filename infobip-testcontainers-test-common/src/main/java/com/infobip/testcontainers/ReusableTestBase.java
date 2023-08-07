db09313035c4dca32fae721e8d3cec60e306a9b0
lpandzic <lovro.pandzic@infobip.com>
2023-08-07 15:48:37 +0200

    fixed ReusableTestBase

--- infobip-testcontainers-test-common/src/main/java/com/infobip/testcontainers/ReusableTestBase.java
+++ infobip-testcontainers-test-common/src/main/java/com/infobip/testcontainers/ReusableTestBase.java
@@ -1,36 +1,39 @@
 package com.infobip.testcontainers;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestConstructor;
+import org.testcontainers.utility.TestcontainersConfiguration;
 
 import java.io.*;
-import java.nio.file.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
-public class ReusableTestBase extends TestBase {
+@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
+@SpringBootTest(classes = Main.class)
+public class ReusableTestBase {
+
+    private static final String HOME = System.getProperty("user.home");
+    private static final String FILENAME = ".testcontainers.properties";
+    private static final File FILE = new File(HOME, FILENAME);
+    private static final File TEMP_FILE = new File(HOME, FILENAME + ".tmp");
 
     @BeforeAll
-    static void setup() {
-        var home = System.getProperty("user.home");
-        var filename = ".testcontainers.properties";
-        var file = new File(home, filename);
-        var tempFile = new File(home, filename + ".tmp");
-        if (file.exists()) {
-            file.renameTo(tempFile);
+    static void createTestcontainersPropertiesFile() {
+        if (FILE.exists()) {
+            FILE.renameTo(TEMP_FILE);
         }
 
-        Path newFile;
-        try {
-            newFile = Files.writeString(Paths.get(home, filename), "testcontainers.reuse.enable=true");
-        } catch (IOException e) {
-            throw new UncheckedIOException(e);
-        }
+        TestcontainersConfiguration.getInstance().updateUserConfig("testcontainers.reuse.enable", "true");
+    }
 
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            try {
-                Files.delete(newFile);
-            } catch (IOException e) {
-                throw new UncheckedIOException(e);
-            }
-            tempFile.renameTo(file);
-        }));
+    @AfterAll
+    static void cleanupTestcontainersPropertiesFile() {
+        FILE.delete();
+
+        if (TEMP_FILE.exists()) {
+            TEMP_FILE.renameTo(FILE);
+        }
     }
 }
