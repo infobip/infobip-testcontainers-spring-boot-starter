893555a79ed242c2562d23d8cc1d90200a7ee50e
lpandzic <lovro.pandzic@infobip.com>
2024-03-11 13:03:01 +0100

    added r2dbc support for mysql

--- infobip-mysql-testcontainers-spring-boot-starter/src/main/java/com/infobip/testcontainers/spring/mysql/MySQLContainerInitializer.java
+++ infobip-mysql-testcontainers-spring-boot-starter/src/main/java/com/infobip/testcontainers/spring/mysql/MySQLContainerInitializer.java
@@ -3,24 +3,28 @@ package com.infobip.testcontainers.spring.mysql;
 import com.infobip.testcontainers.InitializerBase;
 import org.springframework.boot.test.util.TestPropertyValues;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.util.StringUtils;
+import org.springframework.core.env.Environment;
 
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static org.testcontainers.containers.MySQLContainer.MYSQL_PORT;
 
 public class MySQLContainerInitializer extends InitializerBase<MySQLContainerWrapper> {
+
+    private static final List<String> DEFAULT_PROPERTY_NAMES = Arrays.asList("spring.datasource.url",
+                                                                             "spring.flyway.url",
+                                                                             "spring.r2dbc.url");
 
     @Override
     public void initialize(ConfigurableApplicationContext applicationContext) {
         var environment = applicationContext.getEnvironment();
+        var urlPropertyNames = getUrlPropertyNames(environment);
+        var urlPropertyNameToValue = getUrlPropertyNameToValue(environment, urlPropertyNames);
         var dataSourceUrlPropertyName = Optional.ofNullable(
                                                         environment.getProperty("testcontainers.mysql.datasource.url.property.name"))
                                                 .orElse("spring.datasource.url");
-        var dataSourceUrl = environment.getProperty(dataSourceUrlPropertyName);
-
-        if (StringUtils.isEmpty(dataSourceUrl)) {
-            throw new IllegalStateException("URL for test-container is null or empty.");
-        }
-
+        var dataSourceUrl = Objects.requireNonNull(environment.getProperty(dataSourceUrlPropertyName));
         var database = dataSourceUrl.substring(dataSourceUrl.lastIndexOf("/") + 1);
         var wrapper = Optional.ofNullable(environment.getProperty("testcontainers.mysql.docker.image"))
                               .map(imageName -> new MySQLContainerWrapper(database, imageName))
@@ -31,15 +35,70 @@ public class MySQLContainerInitializer extends InitializerBase<MySQLContainerWra
                 .ifPresent(container::withInitScript);
 
         resolveStaticPort(dataSourceUrl, GENERIC_URL_WITH_PORT_GROUP_PATTERN)
-                .ifPresent(staticPort -> bindPort(container, staticPort, MySQLContainerWrapper.MYSQL_PORT));
+                .ifPresent(staticPort -> bindPort(container, staticPort, MYSQL_PORT));
 
         start(container);
 
-        var url = replaceHostAndPortPlaceholders(dataSourceUrl, container, MySQLContainerWrapper.MYSQL_PORT);
-        var values = TestPropertyValues.of(dataSourceUrlPropertyName + "=" + url);
+        var replacedNameToValue = replaceHostAndPort(urlPropertyNameToValue, container);
+        var testPropertyValues = addMissingUsernameAndPassword(replacedNameToValue, container);
+        var values = TestPropertyValues.of(testPropertyValues);
 
         values.applyTo(applicationContext);
 
         registerContainerAsBean(applicationContext);
+    }
+
+    private List<String> getUrlPropertyNames(Environment environment) {
+        var name = environment.getProperty("testcontainers.mysql.datasource.url.property.name");
+        if (Objects.nonNull(name)) {
+            return Collections.singletonList(name);
+        }
+
+        var names = environment.getProperty("testcontainers.mysql.datasource.url.property.names", String[].class);
+        if (Objects.nonNull(names)) {
+            return Arrays.asList(names);
+        }
+
+        return DEFAULT_PROPERTY_NAMES;
+    }
+
+    private Map<String, String> getUrlPropertyNameToValue(Environment environment, List<String> names) {
+        Map<String, String> propertyNameToValue = new HashMap<>();
+
+        for (String name : names) {
+            String value = environment.getProperty(name);
+            if (Objects.nonNull(value)) {
+                propertyNameToValue.put(name, value);
+            }
+        }
+
+        return propertyNameToValue;
+    }
+
+    private Map<String, String> replaceHostAndPort(Map<String, String> urlPropertyNameToValue,
+                                                   MySQLContainerWrapper container) {
+        return urlPropertyNameToValue.entrySet()
+                                     .stream()
+                                     .collect(Collectors.toMap(Map.Entry::getKey,
+                                                               entry -> replaceHostAndPortPlaceholders(entry.getValue(),
+                                                                                                       container,
+                                                                                                       MYSQL_PORT)));
+    }
+
+    private Map<String, String> addMissingUsernameAndPassword(Map<String, String> urlPropertyNameToValue,
+                                                              MySQLContainerWrapper container) {
+        var username = container.getUsername();
+        var password = container.getPassword();
+
+        var testPropertyValues = new HashMap<>(urlPropertyNameToValue);
+        for (var entry : urlPropertyNameToValue.entrySet()) {
+            String name = entry.getKey();
+            if (DEFAULT_PROPERTY_NAMES.contains(name)) {
+                String root = name.substring(0, name.indexOf(".url"));
+                testPropertyValues.put(root + ".username", username);
+                testPropertyValues.put(root + ".password", password);
+            }
+        }
+        return testPropertyValues;
     }
 }
--- infobip-mysql-testcontainers-spring-boot-starter/src/test/java/com/infobip/testcontainers/spring/mysql/MySQLContainerInitializerTest.java
+++ infobip-mysql-testcontainers-spring-boot-starter/src/test/java/com/infobip/testcontainers/spring/mysql/MySQLContainerInitializerTest.java
@@ -4,6 +4,7 @@ import com.infobip.testcontainers.TestBase;
 import lombok.AllArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 import org.springframework.test.context.ActiveProfiles;
@@ -16,6 +17,7 @@ import static org.assertj.core.api.BDDAssertions.then;
 @ActiveProfiles("test")
 class MySQLContainerInitializerTest extends TestBase {
 
+    private final Environment environment;
     private final DataSourceProperties properties;
 
     @Test
@@ -32,6 +34,82 @@ class MySQLContainerInitializerTest extends TestBase {
 
         // then
         then(actual).isEqualTo("TestDatabase");
+    }
+
+    @Test
+    void shouldInsertUsername() {
+        // when
+        String actual = properties.getUsername();
+
+        // then
+        then(actual).isNotBlank();
+    }
+
+    @Test
+    void shouldInsertPassword() {
+        // when
+        String actual = properties.getPassword();
+
+        // then
+        then(actual).isNotBlank();
+    }
+
+    @Test
+    void shouldReplaceHostAndPortInR2dbc() {
+        // when
+        String actual = environment.getProperty("spring.r2dbc.url");
+
+        // then
+        then(actual).isNotBlank()
+                    .doesNotContain("<host>")
+                    .doesNotContain("<port>");
+    }
+
+    @Test
+    void shouldInsertUsernameInR2dbc() {
+        // when
+        String actual = environment.getProperty("spring.r2dbc.username");
+
+        // then
+        then(actual).isNotBlank();
+    }
+
+    @Test
+    void shouldInsertPasswordInR2dbc() {
+        // when
+        String actual = environment.getProperty("spring.r2dbc.password");
+
+        // then
+        then(actual).isNotBlank();
+    }
+
+    @Test
+    void shouldReplaceHostAndPortInFlyway() {
+        // when
+        String actual = environment.getProperty("spring.flyway.url");
+
+        // then
+        then(actual).isNotBlank()
+                    .doesNotContain("<host>")
+                    .doesNotContain("<port>");
+    }
+
+    @Test
+    void shouldInsertUsernameInFlyway() {
+        // when
+        String actual = environment.getProperty("spring.flyway.username");
+
+        // then
+        then(actual).isNotBlank();
+    }
+
+    @Test
+    void shouldInsertPasswordInFlyway() {
+        // when
+        String actual = environment.getProperty("spring.flyway.password");
+
+        // then
+        then(actual).isNotBlank();
     }
 
     private Driver getDriver(String jdbcUrl) {
